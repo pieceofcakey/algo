accad22ca0f5ce0d60efc476866c1c50410dc92b
pieceofcakey <choi_927@naver.com>
2022-05-19 17:07:35 +0900

    짝수와 홀수

--- marathonWalk/q2.js
+++ marathonWalk/q2.js
@@ -1,3 +1,5 @@
+// 짝수와 홀수
+
 // 문제설명
 // 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수,
 // solution을 완성해주세요.
@@ -7,10 +9,11 @@
 // 0은 짝수입니다.
 
 function solution(num) {
-    let answer ='' // answer 값에 빈 문자열 할당, let 선언은 재할당 가능
-    if (num % 2 === 0) { // 매개변수 num의 값이 2로 나눈 나머지가 0일때,
-        return answer = 'Even' // 문자열 'Even' 출력
-    } else {
-        return answer = 'Odd' // 조건문에 충족되지 않으면 문자열 'Odd' 출력
-    }
-} 
+  let answer = ''; // answer 값에 빈 문자열 할당, let 선언은 재할당 가능
+  if (num % 2 === 0) {
+    // 매개변수 num의 값이 2로 나눈 나머지가 0일때,
+    return (answer = 'Even'); // 문자열 'Even' 출력
+  } else {
+    return (answer = 'Odd'); // 조건문에 충족되지 않으면 문자열 'Odd' 출력
+  }
+}
