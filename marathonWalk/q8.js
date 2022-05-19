33ae12e18b90f287a51a013ae985db0229d39c33
pieceofcakey <choi_927@naver.com>
2022-05-19 17:08:17 +0900

    평균 구하기

--- marathonWalk/q8.js
+++ marathonWalk/q8.js
@@ -1,9 +1,21 @@
-function solution(arr) {
-    let answer = 0;
-    let sum = 0
+// 평균 구하기
 
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i]
-    }
-    return answer = sum / arr.length
+// 문제 설명
+// 정수를 담고 있는 배열 arr의 평균값을 return하는 함수,
+//  solution을 완성해보세요.
+
+// 제한사항
+// arr은 길이 1 이상, 100 이하인 배열입니다.
+// arr의 원소는 -10,000 이상 10,000 이하인 정수입니다.
+
+function solution(arr) {
+  let answer = 0;
+  for (let i = 0; i < arr.length; i++) {
+    answer += arr[i];
+  }
+  return answer / arr.length;
 }
+
+// 테스트
+// let a = [1, 2, 3, 4];
+// console.log(a);
