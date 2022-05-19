eb246f25104cff56063686b806101e016bfa7e90
pieceofcakey <choi_927@naver.com>
2022-05-19 17:11:26 +0900

    문자열을 정수로 바꾸기

--- marathonWalk/q5.js
+++ marathonWalk/q5.js
@@ -1,4 +1,4 @@
-// 문자열을 정수로 바꾸기
+//  문자열을 정수로 바꾸기
 
 // 문제 설명
 // 문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요
@@ -9,10 +9,8 @@
 // s는 부호와 숫자로만 이루어져있습니다.
 // s는 "0"으로 시작하지 않습니다.
 
-
-
-function solution(s){
-    return Number(s)
+function solution(s) {
+  return Number(s);
 }
 
 // 문자열을 숫자로 바꾸는 Number(); 에 대해서 배우게 되는 시간이 됐습니다.
