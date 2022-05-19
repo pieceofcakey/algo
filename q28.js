95d7e64b91750a7a3eb3bf14d21c9c162f8c17d1
pieceofcakey <choi_927@naver.com>
2022-05-19 12:06:55 +0900

    하샤드 수

---
+++ q28.js
@@ -0,0 +1,25 @@
+// 하샤드 수
+
+// 문제 설명
+// 양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다.
+// 예를 들어 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로
+// 18은 하샤드 수입니다. 자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수,
+// solution을 완성해주세요.
+
+// 제한 조건
+// x는 1 이상, 10000 이하인 정수입니다.
+
+function solution(x) {
+  // x = 18
+  let sum = 0;
+  let arr = String(x).split(''); // [1,8]
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += Number(arr[i]); // 1+8
+  }
+  return x % sum === 0 ? true : false;
+  // x가 sum으로 나누어떨어지면 true,아니면 false
+}
+
+// 테스트
+console.log(solution(18));
