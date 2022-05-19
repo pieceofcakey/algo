81a39dacc2c0da5e3f38149a2eafbb27dff7ac47
pieceofcakey <choi_927@naver.com>
2022-05-19 17:11:42 +0900

    부족한 금액 계산하기

--- marathonWalk/q12.js
+++ marathonWalk/q12.js
@@ -1,11 +1,13 @@
-// // 문제 설명
-// // 새로 생긴 놀이기구는 인기가 매우 많아 줄이 끊이질 않습니다. 
-// 이 놀이기구의 원래 이용료는 price원 인데, 놀이기구를 N 번 째 이용한다면 
-// 원래 이용료의 N배를 받기로 하였습니다. 
+// 부족한 금액 계산하기
+
+// 문제 설명
+// 새로 생긴 놀이기구는 인기가 매우 많아 줄이 끊이질 않습니다.
+// 이 놀이기구의 원래 이용료는 price원 인데, 놀이기구를 N 번 째 이용한다면
+// 원래 이용료의 N배를 받기로 하였습니다.
 // 즉, 처음 이용료가 100이었다면 2번째에는 200, 3번째에는 300으로 요금이 인상됩니다.
-// // 놀이기구를 count번 타게 되면 현재 자신이 가지고 있는 금액에서
+// 놀이기구를 count번 타게 되면 현재 자신이 가지고 있는 금액에서
 //  얼마가 모자라는지를 return 하도록 solution 함수를 완성하세요.
-// // 단, 금액이 부족하지 않으면 0을 return 하세요.
+// 단, 금액이 부족하지 않으면 0을 return 하세요.
 
 // 제한사항
 // 놀이기구의 이용료 price : 1 ≤ price ≤ 2,500, price는 자연수
@@ -13,14 +15,16 @@
 // 놀이기구의 이용 횟수 count : 1 ≤ count ≤ 2,500, count는 자연수
 
 function solution(price, money, count) {
-    let totalPrice = 0; // 우선 총 금액변수 totalPrice 할당
-    for (let i = 1; i <= count; i++) { // count는 1부터 시작하는 자연수, i = 1 부터 count 만큼 반복
-      totalPrice += price * i // totalPrice = totalPrice + (price * i) i번 count 한 금액을 더해서 totalPrice에 재할당
-    }
-    if (totalPrice <= money) { // 총금액이 현재 가진금액보다 낮으면 모자란돈은 0원
-        return 0
-    } 
-    return totalPrice - money // 총금액이 현재 가진금액보다 높으면 모자란돈은 totalPrice - money
+  let totalPrice = 0; // 우선 총 금액변수 totalPrice 할당
+  for (let i = 1; i <= count; i++) {
+    // count는 1부터 시작하는 자연수, i = 1 부터 count 만큼 반복
+    totalPrice += price * i; // totalPrice = totalPrice + (price * i) i번 count 한 금액을 더해서 totalPrice에 재할당
+  }
+  if (totalPrice <= money) {
+    // 총금액이 현재 가진금액보다 낮으면 모자란돈은 0원
+    return 0;
+  }
+  return totalPrice - money; // 총금액이 현재 가진금액보다 높으면 모자란돈은 totalPrice - money
 }
 
 // console.log(solution(100, 1000 ,5)) 테스트 출력
